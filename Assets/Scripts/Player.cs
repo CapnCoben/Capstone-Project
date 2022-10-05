0d0d29bf0eab2c56c8e61569394f503f4937151b
Scott Fisk <67887150+scott-fisk@users.noreply.github.com>
2022-10-05 19:06:39 -0400

    Updated Character Selection
    Character Selection added to UI. When User selects a character, the UI button changes from select to selected. The choice is saved in PlayerPrefs.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace IBR
+{
+    public class Player : MonoBehaviour
+    {
+        public int characterValue;
+        public Button characterButton;
+        public GameObject selectedCharacter;
+
+
+        PlayerSelectedState playerSelection;
+
+
+        public void Select()
+        {
+            selectedCharacter.gameObject.SetActive(true);
+
+            CharacterSelection.Instance.SelectNewCharacter(characterValue);
+        }
+
+    }
+
+    public enum PlayerSelectedState
+    {
+        Selected,
+        Unselected
+    }
+
+}
