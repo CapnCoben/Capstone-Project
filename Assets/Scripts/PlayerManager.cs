486d6bba8159208f60457639850eccc57d0e0ac1
Scott Fisk <67887150+scott-fisk@users.noreply.github.com>
2022-10-06 22:08:03 -0400

    Loads the prefab character into the scene
    Olivia will be instantiated into the scene.

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -14,6 +14,10 @@ namespace IBR
     {
         public float Health = 1f;
         //bool IsFiring;
+
+        [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
+        public static GameObject LocalPlayerInstance;
+
         #region IPunObservable implementation
 
 
@@ -41,7 +45,15 @@ namespace IBR
         /// </summary>
         void Awake()
         {
-            
+            // #Important
+            // used in GameManager.cs: we keep track of the localPlayer instance to prevent instantiation when levels are synchronized
+            if (photonView.IsMine)
+            {
+                PlayerManager.LocalPlayerInstance = this.gameObject;
+            }
+            // #Critical
+            // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
+            DontDestroyOnLoad(this.gameObject);
         }
 
         /// <summary>
