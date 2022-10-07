ec388933fa4c6e99e42b76fe68defa140ffb7988
Scott Fisk <67887150+scott-fisk@users.noreply.github.com>
2022-10-06 22:19:47 -0400

    PlayerUI.cs
    UI script to control player name and health.

---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace IBR
+{
+    public class PlayerUI : MonoBehaviour
+    {
+        #region Private Fields
+
+
+        [Tooltip("UI Text to display Player's Name")]
+        [SerializeField]
+        private Text playerNameText;
+
+
+        [Tooltip("UI Slider to display Player's Health")]
+        [SerializeField]
+        private Slider playerHealthSlider;
+
+
+        #endregion
+
+
+        #region MonoBehaviour Callbacks
+
+
+        #endregion
+
+
+        #region Public Methods
+
+
+        #endregion
+
+
+    }
+}
