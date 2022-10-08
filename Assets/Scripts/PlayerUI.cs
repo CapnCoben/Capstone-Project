a1d2273fd00a958ced69f088f04214307677250c
Scott Fisk <67887150+scott-fisk@users.noreply.github.com>
2022-10-08 12:54:30 -0400

    Update PlayerUI.cs
    Comment out void LateUpdate()

--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -58,24 +58,24 @@ namespace IBR
                 return;
             }
 
-            void LateUpdate()
-            {
-                // Do not show the UI if we are not visible to the camera, thus avoid potential bugs with seeing the UI, but not the player itself.
-                if (targetRenderer != null)
-                {
-                    this._canvasGroup.alpha = targetRenderer.isVisible ? 1f : 0f;
-                }
+            //void LateUpdate()
+            //{
+            //    // Do not show the UI if we are not visible to the camera, thus avoid potential bugs with seeing the UI, but not the player itself.
+            //    if (targetRenderer != null)
+            //    {
+            //        this._canvasGroup.alpha = targetRenderer.isVisible ? 1f : 0f;
+            //    }
 
 
-                // #Critical
-                // Follow the Target GameObject on screen.
-                if (targetTransform != null)
-                {
-                    targetPosition = targetTransform.position;
-                    targetPosition.y += characterControllerHeight;
-                    this.transform.position = Camera.main.WorldToScreenPoint(targetPosition) + screenOffset;
-                }
-            }
+            //    // #Critical
+            //    // Follow the Target GameObject on screen.
+            //    if (targetTransform != null)
+            //    {
+            //        targetPosition = targetTransform.position;
+            //        targetPosition.y += characterControllerHeight;
+            //        this.transform.position = Camera.main.WorldToScreenPoint(targetPosition) + screenOffset;
+            //    }
+            //}
 
         }
 
