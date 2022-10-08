58c2897c65b5a9ec8cf808727e6340c74a303c39
Scott Fisk <67887150+scott-fisk@users.noreply.github.com>
2022-10-08 17:00:23 -0400

    Update PlayerUI.cs

--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -45,11 +45,11 @@ namespace IBR
 
         void Update()
         {
-            // Reflect the Player Health
-            //if (playerHealthSlider != null)
-            //{
-            //    playerHealthSlider.value = target.Health;
-            //}
+            //Reflect the Player Health
+            if (playerHealthSlider != null)
+            {
+                playerHealthSlider.value = target.Health;
+            }
 
             // Destroy itself if the target is null, It's a fail safe when Photon is destroying Instances of a Player over the network
             if (target == null)
@@ -58,24 +58,24 @@ namespace IBR
                 return;
             }
 
-            //void LateUpdate()
-            //{
-            //    // Do not show the UI if we are not visible to the camera, thus avoid potential bugs with seeing the UI, but not the player itself.
-            //    if (targetRenderer != null)
-            //    {
-            //        this._canvasGroup.alpha = targetRenderer.isVisible ? 1f : 0f;
-            //    }
+            void LateUpdate()
+            {
+                // Do not show the UI if we are not visible to the camera, thus avoid potential bugs with seeing the UI, but not the player itself.
+                if (targetRenderer != null)
+                {
+                    this._canvasGroup.alpha = targetRenderer.isVisible ? 1f : 0f;
+                }
 
 
-            //    // #Critical
-            //    // Follow the Target GameObject on screen.
-            //    if (targetTransform != null)
-            //    {
-            //        targetPosition = targetTransform.position;
-            //        targetPosition.y += characterControllerHeight;
-            //        this.transform.position = Camera.main.WorldToScreenPoint(targetPosition) + screenOffset;
-            //    }
-            //}
+                // #Critical
+                // Follow the Target GameObject on screen.
+                if (targetTransform != null)
+                {
+                    targetPosition = targetTransform.position;
+                    targetPosition.y += characterControllerHeight;
+                    this.transform.position = Camera.main.WorldToScreenPoint(targetPosition) + screenOffset;
+                }
+            }
 
         }
 
